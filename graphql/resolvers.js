cd11fa0efb9648322d793c974ba803aa6dd735e6
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-06-03 11:09:04 +0200

    Adding a login query & resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -4,6 +4,7 @@
  */
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 /**
  * Local import
@@ -57,5 +58,35 @@ module.exports = {
     // Returning the User object
     // ._doc delete all the meta data mongoose creates
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+
+  login: async ({ email, password }) => {
+    // Finding the user
+    const user = await User.findOne({ email });
+    // If no user exists, throw an error
+    if (!user) {
+      const error = new Error('User not found');
+      error.code = 401;
+      throw error;
+    }
+    // Comparing passwords
+    const passwordsMatched = await bcrypt.compare(password, user.password);
+    // If passwords don't match, throw an error
+    if (!passwordsMatched) {
+      const error = new Error('Password is incorrect');
+      error.code = 401;
+      throw error;
+    }
+    // Generating a token
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      'secret',
+      { expiresIn: '1h' }
+    );
+    // Return a token & userId
+    return { token, userId: user._id.toString() };
   },
 };
